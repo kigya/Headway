fa3e7acd113cfc09784c308007a8f008fe6c8097
BashlikovV <bashlikovvaldimir@gmail.com>
2025-12-19 15:05:49 +0300

    fix(refactor): Removed unused data classes.

--- server/auth/src/main/kotlin/dev/kigya/headway/auth/data/database/request/RefreshRequest.kt
+++
@@ -1,9 +0,0 @@
-package dev.kigya.headway.auth.data.database.request
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-internal data class RefreshRequest(
-    @SerialName("refresh_token") val refreshToken: String,
-)
